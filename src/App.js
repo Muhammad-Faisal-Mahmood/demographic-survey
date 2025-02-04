de9ad9b8d8b466a4975e99eb83ffaf91b4fa8f22
Muhammad-Faisal-Mahmood <mfaisalm1000@gmail.com>
2025-02-05 00:34:02 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import Home from './containers/Home';
-import About from './containers/About';
-import ImplicitBiasTest from './containers/ImplicitBiasTest';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "./containers/Home";
+import About from "./containers/About";
+import ImplicitBiasTest from "./containers/ImplicitBiasTest";
+import DemographicInfo from "./containers/DemographicInfo";
+import "./scss/global.scss";
 
 export default function App() {
   return (
     <Router>
       <div className="nav-and-content">
-        
         <Switch>
           <Route path={`/test/:testId`}>
             <ImplicitBiasTest />
@@ -21,15 +17,14 @@ export default function App() {
           <Route path="/about">
             <About />
           </Route>
-          <Route path="/">
+          <Route path="/home">
             <Home />
           </Route>
+          <Route path="/">
+            <DemographicInfo />
+          </Route>
         </Switch>
-
-        
-
       </div>
-
     </Router>
   );
 }
--- src/containers/ImplicitBiasTest.js
+++ src/containers/ImplicitBiasTest.js
@@ -1,38 +1,45 @@
-import React from 'react';
-import { connect } from 'react-redux'
-import { useParams } from 'react-router-dom'
+import React from "react";
+import { connect } from "react-redux";
+import { useParams } from "react-router-dom";
 
-import { startTest } from '../actions'
-import Block from '../components/Block'
-import Results from '../components/Results'
-import TestNotFound from '../components/TestNotFound'
-import TestStart from '../components/TestStart'
-import tests from '../data/tests'
-import '../scss/implicit-bias-test.scss'
+import { startTest } from "../actions";
+import Block from "../components/Block";
+import Results from "../components/Results";
+import TestNotFound from "../components/TestNotFound";
+import TestStart from "../components/TestStart";
+import tests from "../data/tests";
+import "../scss/implicit-bias-test.scss";
 
 const ImplicitBiasTest = ({ currentTest, dispatchStartTest }) => {
   const { testId } = useParams();
   const test = tests.find((test) => test.id === testId);
-  if (!test) return (<TestNotFound />)
+  if (!test) return <TestNotFound />;
   if (!!currentTest) {
-    return !currentTest.finished ?
-      <Block test={test} currentTest={currentTest} /> :
+    return !currentTest.finished ? (
+      <Block test={test} currentTest={currentTest} />
+    ) : (
       <Results test={test} currentTest={currentTest} />
+    );
   }
-  return (<TestStart test={test} onStartTestButtonPress={() => dispatchStartTest(test)} />)
-}
+  return (
+    <TestStart
+      test={test}
+      onStartTestButtonPress={() => dispatchStartTest(test)}
+    />
+  );
+};
 
 const mapStateToProps = ({ currentTest }) => ({
-  currentTest
-})
+  currentTest,
+});
 
-const mapDispatchToProps = dispatch => ({
-  dispatchStartTest: test => dispatch(startTest(test))
-})
+const mapDispatchToProps = (dispatch) => ({
+  dispatchStartTest: (test) => dispatch(startTest(test)),
+});
 
 const VisibleImplicitBiasTest = connect(
   mapStateToProps,
   mapDispatchToProps
-)(ImplicitBiasTest)
+)(ImplicitBiasTest);
 
-export default VisibleImplicitBiasTest
+export default VisibleImplicitBiasTest;
